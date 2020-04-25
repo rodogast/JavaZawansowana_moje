023dd87798a46ff719cca98158cb012f6934d590
rodogast <rodogast@gmail.com>
2020-04-25 08:40:23 +0200

    a

---
+++ src/main/java/MojeMain.java
@@ -0,0 +1,17 @@
+public class MojeMain {
+    public static void main(String[] args) {
+        String a="dupa";
+        int b=2;
+
+        if(b>0){
+
+        for( int i=b;i<10;i--){
+
+        }
+        };
+
+
+
+    }
+
+}
